bc9e4baa6cdc54ff21df64392b5d6c790bf15784
Vivek N <rep.movsd@gmail.com>
2020-12-21 20:41:14 +0530

    Make command print args as JSON

--- print.sh
+++ print.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "$*"
+echo -n "{"
+while (( "$#" >= 1 )); do
+  if [[ $1 == \-* ]] ;
+  then
+    KEY=$1
+    if [[ $2 == \-* ]] ;
+    then
+      VAL=''
+      shift
+    else
+      VAL=$2
+      shift 2
+    fi
+    echo -n \"${KEY}\"\: \"${VAL}\"
+    (( "$#" > 0 )) && echo -n ","
+  else
+    shift
+  fi
+done
+echo "}"
